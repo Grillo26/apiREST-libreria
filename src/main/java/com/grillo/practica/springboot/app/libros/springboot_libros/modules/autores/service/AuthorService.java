0d0296980bb2c5085fabaceba457d0043e67fb4e
Grillo <carlsenrmt26@gmail.com>
2026-07-17 20:31:35 -0400

    feat: Crud completo ed autores

--- src/main/java/com/grillo/practica/springboot/app/libros/springboot_libros/modules/autores/service/AuthorService.java
+++ src/main/java/com/grillo/practica/springboot/app/libros/springboot_libros/modules/autores/service/AuthorService.java
@@ -1,9 +1,11 @@
 package com.grillo.practica.springboot.app.libros.springboot_libros.modules.autores.service;
 
+import com.grillo.practica.springboot.app.libros.springboot_libros.common.dto.ResourceNotFoundException;
 import com.grillo.practica.springboot.app.libros.springboot_libros.modules.autores.dto.AuthorRequestDTO;
 import com.grillo.practica.springboot.app.libros.springboot_libros.modules.autores.dto.AuthorResponseDTO;
 import com.grillo.practica.springboot.app.libros.springboot_libros.modules.autores.model.AuthorEntity;
 import com.grillo.practica.springboot.app.libros.springboot_libros.modules.autores.repository.AuthorRepository;
+import com.grillo.practica.springboot.app.libros.springboot_libros.modules.cliente.dto.ClienteRequestDTO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -44,6 +46,38 @@ public class AuthorService {
                 .nombre(authorGuardado.getNombre())
                 .nacionalidad(authorGuardado.getNacionalidad())
                 .build();
+    }
+
+    @Transactional
+    public AuthorResponseDTO editarAutor(Long id, AuthorRequestDTO requestDTO){
+
+        // 1.- Primero buscamos si existe en la base de datos
+        AuthorEntity autorDb = authorRepository.findById(id)
+                .orElseThrow( () -> new ResourceNotFoundException("Author no Encontrado"));
+
+        // 2.- Modificar
+        autorDb.setNombre(requestDTO.getNombre());
+        autorDb.setNacionalidad(requestDTO.getNacionalidad());
+
+        // 3.- Guardar
+        authorRepository.save(autorDb);
+
+        // 4.- Devolvemos el DTO
+        return AuthorResponseDTO.builder()
+                .id(autorDb.getId())
+                .nombre(autorDb.getNombre())
+                .nacionalidad(autorDb.getNacionalidad())
+                .build();
+    }
+
+    // Esta vez haremos el hard delete
+    @Transactional
+    public void eliminarAutor(Long id){
+         // 1.- Verificar primero si existe
+        if(!authorRepository.existsById(id)){
+            throw new ResourceNotFoundException("Autor no encontrado con el id: " +id);
+        }
+        authorRepository.deleteById(id);
     }
 
 
